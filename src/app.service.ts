d1a4ce30524e990973822720b8e26a239b7da485
powerlimit <ivanbondar133@gmail.com>
2024-02-10 13:04:27 +0200

    add prisma service

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(private prisma: PrismaService) {}
+
+  getHello() {
+    return 'hello';
   }
 }
